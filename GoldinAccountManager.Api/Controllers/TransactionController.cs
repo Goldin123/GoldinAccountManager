65269df26c370b24c7e8479bc4e1f3b91726f3d9
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-06-28 13:06:31 +0200

    SCBSEGB-1 added JWT auth to the API

--- GoldinAccountManager.Api/Controllers/TransactionController.cs
+++ GoldinAccountManager.Api/Controllers/TransactionController.cs
@@ -1,11 +1,13 @@
 ﻿using GoldinAccountManager.Database.Interface;
 using GoldinAccountManager.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GoldinAccountManager.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class TransactionController : ControllerBase
