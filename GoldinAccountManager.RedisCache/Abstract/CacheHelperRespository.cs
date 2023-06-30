afb6d2eeddaa8cbad2cdf7ec4500091fba0b9508
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-07-01 00:07:59 +0200

    SCBSEGB-4 added redis repository

---
+++ GoldinAccountManager.RedisCache/Abstract/CacheHelperRespository.cs
@@ -0,0 +1,13 @@
+﻿using GoldinAccountManager.RedisCache.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldinAccountManager.RedisCache.Abstract
+{
+    public class CacheHelperRespository : ICacheHelperRespository
+    {
+    }
+}
