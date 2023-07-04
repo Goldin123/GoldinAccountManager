2fbe8a732d63400a3ed3f9adeb76cd6ad76daf3e
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-07-04 12:10:35 +0200

    added sample data

--- GoldinAccountManager.Database/Interface/IAccountRepository.cs
+++ GoldinAccountManager.Database/Interface/IAccountRepository.cs
@@ -12,7 +12,7 @@ namespace GoldinAccountManager.Database.Interface
         Task<List<Account>> AddAccountsAsync(List<AccountRequest> account);
         Task<Account> AddAccountAsync(AccountRequest account);
         Task<List<Account>> GetAllAccountsAsync();
-        Task<Account> GetAccountByIdAsync(string id);  
+        Task<Account> GetAccountByIdentityNumberAsync(string id);  
         Task<Account> GetAccountByIdAsync(int id);
         Task<Account> UpdateAccountAsync(AccountRequest account);
         Task UpdateAccountBalanceAsync(int accountId, decimal amount, TransactionType transactionType);
